cbd17bddf56359a8a4c33ec210de3bb9a5692713
Nixie <mgmbautista@gmail.com>
2025-07-24 09:57:29 -0600

    Se agrego codigo funcional

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,53 +29,66 @@ namespace Ejem2FormAnioBisiesto
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnVerificar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtAnioSaber = new System.Windows.Forms.TextBox();
+            this.txtMostrar = new System.Windows.Forms.TextBox();
+            this.btnFin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnVerificar
             // 
-            this.button1.Location = new System.Drawing.Point(676, 208);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnVerificar.Location = new System.Drawing.Point(431, 125);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(129, 61);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "VERIFICAR";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(81, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(156, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Ingrese el año que desea saber";
             // 
-            // textBox1
+            // txtAnioSaber
             // 
-            this.textBox1.Location = new System.Drawing.Point(280, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtAnioSaber.Location = new System.Drawing.Point(280, 74);
+            this.txtAnioSaber.Name = "txtAnioSaber";
+            this.txtAnioSaber.Size = new System.Drawing.Size(100, 20);
+            this.txtAnioSaber.TabIndex = 2;
             // 
-            // textBox2
+            // txtMostrar
             // 
-            this.textBox2.Location = new System.Drawing.Point(125, 208);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(281, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtMostrar.Location = new System.Drawing.Point(84, 204);
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(281, 20);
+            this.txtMostrar.TabIndex = 3;
+            // 
+            // btnFin
+            // 
+            this.btnFin.Location = new System.Drawing.Point(561, 240);
+            this.btnFin.Name = "btnFin";
+            this.btnFin.Size = new System.Drawing.Size(75, 23);
+            this.btnFin.TabIndex = 4;
+            this.btnFin.Text = "Finalizar";
+            this.btnFin.UseVisualStyleBackColor = true;
+            this.btnFin.Click += new System.EventHandler(this.btnFin_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(644, 280);
+            this.Controls.Add(this.btnFin);
+            this.Controls.Add(this.txtMostrar);
+            this.Controls.Add(this.txtAnioSaber);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnVerificar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -85,10 +98,11 @@ namespace Ejem2FormAnioBisiesto
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnVerificar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtAnioSaber;
+        private System.Windows.Forms.TextBox txtMostrar;
+        private System.Windows.Forms.Button btnFin;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Ejem2FormAnioBisiesto
 {
+    //Diseño del formulario para Año Bisiesto.
+
+
+    //Creado por NixieNixi
+    //Motivo: Pratica de ESD115, CICLO II - 2025
+    //Fecha de creacion y finalizacion: 24/07/2025
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+               // anio = int.Parse(txtAnioSaber.Text); (Esto es el original pero ami me da error asi que opte por agg otra funcion
+
+            // Funcion agg (Agg aparte)
+            if (!int.TryParse(txtAnioSaber.Text, out int anio))
+            {
+                MessageBox.Show("Ingrese un anio valido");
+                return;
+            }
+
+            if (EsBisiesto(anio)) //Verificamos si el anio es bisiesto(aqui mandamos a llamar a la funcion
+            {
+                txtMostrar.Text = "El Anio" + " " + anio + " " + "Si es Bisiesto";
+
+            }
+            else
+            {
+                txtMostrar.Text = "El Anio" + " " + anio + " " + "  No es Bisiesto";
+            }
+
+            txtAnioSaber.Focus();
+            txtAnioSaber.Clear();
+        }
+
+        static bool EsBisiesto(int anio)
+        {
+            if(anio %4 == 0)
+            {
+                if(anio %100 == 0)
+                {
+                    if (anio % 400==0)
+                    {
+                        return true;
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btnFin_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
